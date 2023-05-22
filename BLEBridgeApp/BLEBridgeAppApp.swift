372874624b1fa2cfb18e02f7da3cb6197160faa9
Roman Andrykevych <romiroma@icloud.com>
2023-05-23 00:31:24 +0300

    Initial Commit

---
+++ BLEBridgeApp/BLEBridgeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLEBridgeAppApp.swift
+//  BLEBridgeApp
+//
+//  Created by Roman on 23.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct BLEBridgeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
