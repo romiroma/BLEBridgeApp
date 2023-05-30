c02d367e1e3a4fc9455d8a8a92cd41dd40e44ab8
Roman Andrykevych <romiroma@icloud.com>
2023-05-30 23:56:20 +0300

    Implements basic peripheral discover

--- BLEBridgeApp/BLEBridgeAppApp.swift
+++ BLEBridgeApp/BLEBridgeAppApp.swift
@@ -1,17 +1,20 @@
-//
-//  BLEBridgeAppApp.swift
-//  BLEBridgeApp
-//
-//  Created by Roman on 23.05.2023.
-//
 
+import BLEBridge
+import BLEBridgeLive
+import BLEBridgeUI
+import ComposableArchitecture
 import SwiftUI
 
 @main
 struct BLEBridgeAppApp: App {
+
+    let store: StoreOf<BLEBridge> = .live()
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+
+            BLEBridgeView(store: store)
         }
+        .windowToolbarStyle(.unifiedCompact(showsTitle: false))
     }
 }
